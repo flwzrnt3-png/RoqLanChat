e94cb69b2dfb3080399566d176755f1933d1d28a
flwzrnt3-png <flwzrnt3@gmail.com>
2026-06-14 14:00:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "roviq.support@gmail.com"
-app.config["MAIL_PASSWORD"] = "valf djav qhqw tfyi"
+app.config["MAIL_PASSWORD"] = "valfdjavqhqwtfyi"
 
 mail = Mail(app)
 
